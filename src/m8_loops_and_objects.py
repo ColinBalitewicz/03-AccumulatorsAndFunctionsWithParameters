a79a8dd1b826c13ee69baef56393dc0b1b6fe414
ColinBalitewicz <balitecd@rose-hulman.edu>
2019-01-28 10:45:17 -0500

    K

--- src/m8_loops_and_objects.py
+++ src/m8_loops_and_objects.py
@@ -6,18 +6,22 @@ This module demonstrates simple LOOPS of the form:
 and also USING OBJECTS.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Colin Balitewicz.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
 
 def main():
+    print_sequence1(20)
+    draw_circles1(21)
     """ Calls the other functions to demonstrate and/or test them. """
     # Test your functions by putting calls to them here:
 
 
-def print_sequence1():
+def print_sequence1(n):
+    for k in range(n+1):
+        print(k*10)
     """
     Prints:
        0
@@ -29,7 +33,7 @@ def print_sequence1():
        200
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement this function, per its doc-string above.
+    # DONE: 2. Implement this function, per its doc-string above.
     # Put a statement in  main  to test this function.
     # REQUIREMENT: You must use a   RANGE  statement to solve this problem.
     # -------------------------------------------------------------------------
@@ -39,7 +43,15 @@ def print_sequence1():
     print('--------------------------------------------------')
 
 
-def draw_circles1():
+def draw_circles1(n):
+    window=rg.RoseWindow(400,400)
+    window.render()
+    for k in range(n+1):
+        circle=rg.Circle(rg.Point(200,200),k*10)
+        circle.attach_to(window)
+        window.render()
+    window.close_on_mouse_click()
+
     """
     -- Constructs an rg.RoseWindow whose width and height are both 400.
     -- Constructs and draws 21 rg.Circle objects such that:
